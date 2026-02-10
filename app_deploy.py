41912ba7fc3bff6f3333fc732d24d5bb148ab2c4
MINKYEONG KIM <mik1970813@gmail.com>
2026-02-10 13:36:37 +0900

    Add gspread and Google credentials imports

--- app_deploy.py
+++ app_deploy.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
 import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
 import os
 import hashlib
 import html as html_lib
